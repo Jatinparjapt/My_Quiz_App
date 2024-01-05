bd526d89830a1fc43c19e59810473917002b497f
jatinkumar <jatin3961jk@gmail.com>
2024-01-05 23:00:15 +0530

    jatinji

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@
  */
 const nextConfig = {
   output: 'export',
+  pages: async () => {
+    return {
+      '/': { page: './pages/index.js' },
+      // Exclude the problematic page
+      // '/quizPages/myQuiz': { do not include this entry },
+    };
+  },
 }
  
 module.exports = nextConfig
