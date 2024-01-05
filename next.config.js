10d38c046c8e6ac0d1e6188982a4f5e272bd6079
jatinkumar <jatin3961jk@gmail.com>
2024-01-05 19:15:31 +0530

    pages

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@
 module.exports =  {exportPathMap: async function () {
   return {
     '/': { page: './pages/index.js' },  // Example: Map the root URL to the index page
+    '/createQuiz': { page: './pages/quizPages/createQuiz.js' },
+    '/myQuiz': { page: './pages/quizPages/myQuiz.js' },
+    '/playQuiz': { page: './pages/quizPages/playQuiz.js' },
+    '/resultQuiz': { page: './pages/quizPages/resultQuiz.js' },
   };
 },}
