db320c55f746cb7220650588e95b1197bd2a1fff
jatinkumar <jatin3961jk@gmail.com>
2024-01-05 19:35:37 +0530

    jaa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-
-module.exports =  { output: {
-  // Use the following line for static HTML export
-  pages: async () => {
-    return {
-      '/': { page: './pages/index.js' },  // Example: Map the root URL to the index page
-      '/createQuiz': { page: './pages/quizPages/createQuiz.js' },
-      '/myQuiz': { page: './pages/quizPages/myQuiz.js' },
-      '/playQuiz': { page: './pages/quizPages/playQuiz.js' },
-      '/resultQuiz': { page: './pages/quizPages/resultQuiz.js' },
-    };
-  },
-},}
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+}
+ 
+module.exports = nextConfig
